d044b6680898f95330bc4963166faea72d66e0dd
vadimoffski <hryshchenko95@gmail.com>
2021-12-20 23:56:13 +0200

    changed task-10

--- task-10.js
+++ task-10.js
@@ -1,16 +1,27 @@
 import users from "./users.js";
 //Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 const getSortedUniqueSkills = (users) => {
-  let totalSkills = [];
-  users.forEach(({ skills }) => totalSkills.push(...skills));
-  totalSkills.sort();
-  let res = [];
-  totalSkills.forEach((el) => {
-    if (!res.includes(el)) {
-      res.push(el);
-    }
-  });
-  return res;
+  let unique = [];
+  users
+    .reduce((acc, { skills }) => acc.concat(skills), [])
+    .sort()
+    .forEach((el) => {
+      if (!unique.includes(el)) {
+        unique.push(el);
+      }
+    });
+  return unique; //не нашел другого варианта заменить forEach...
+
+  // let totalSkills = [];
+  // users.forEach(({ skills }) => totalSkills.push(...skills));
+  // totalSkills.sort();
+  // let res = [];
+  // totalSkills.forEach((el) => {
+  //   if (!res.includes(el)) {
+  //     res.push(el);
+  //   }
+  // });
+  // return res;
 };
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
