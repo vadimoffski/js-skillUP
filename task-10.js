1d632da5593f11036f4ae199c1d1af94effd5452
vadimoffski <hryshchenko95@gmail.com>
2021-12-21 21:39:10 +0200

    added method filter

--- task-10.js
+++ task-10.js
@@ -1,27 +1,12 @@
 import users from "./users.js";
 //Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 const getSortedUniqueSkills = (users) => {
-  let unique = [];
-  users
+  return users
     .reduce((acc, { skills }) => acc.concat(skills), [])
     .sort()
-    .forEach((el) => {
-      if (!unique.includes(el)) {
-        unique.push(el);
-      }
-    });
-  return unique; //не нашел другого варианта заменить forEach...
-
-  // let totalSkills = [];
-  // users.forEach(({ skills }) => totalSkills.push(...skills));
-  // totalSkills.sort();
-  // let res = [];
-  // totalSkills.forEach((el) => {
-  //   if (!res.includes(el)) {
-  //     res.push(el);
-  //   }
-  // });
-  // return res;
+    .filter((el, idx, arr) => idx === arr.indexOf(el));
 };
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+
+//['adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'culpa', 'elit', 'ex', 'ipsum', 'ipsum', 'ipsum', 'irure', 'laborum', 'lorem', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam', 'veniam']
